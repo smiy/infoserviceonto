164a5369d07409721e53c5b0a6e278863cb6a318
Bo Ferri <zazi@smiy.org>
2010-07-11 16:18:00 +0000

    infoserviceonto: * small typo issue

--- examples/gen_turtle.sh
+++ examples/gen_turtle.sh
@@ -5,4 +5,4 @@
 rapper -i rdfxml -o turtle RDF/infoservicetypes.rdf > N3/infoservicetypes.n3
 rapper -i rdfxml -o turtle RDF/infoservicecontributortypes.rdf > N3/infoservicecontributortypes.n3
 rapper -i rdfxml -o turtle RDF/infoservicequalitytypes.rdf > N3/infoservicequalitytypes.n3
-rapper -i rdfxml -o turtle RDF/infoserviceindividuals.rdf > N3/infoserviceindividuals.m3
+rapper -i rdfxml -o turtle RDF/infoserviceindividuals.rdf > N3/infoserviceindividuals.n3
